c7ed0680227ad09f4141d6a29eab77cc3f0604a1
jonatan-sv <jonatan.vidal@alu.ufc.br>
2024-08-20 16:23:06 -0300

    refactor(backend): adiciona detalhes da conexão do db ao log

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,15 @@ const url = "mongodb://localhost:27017/Pokedex";
 const connectDB = async () => {
   try {
     await mongoose.connect(url);
-    logger.info("[DB] Conectado ao MongoDB com sucesso!");
+
+    const info = {
+      db: mongoose.connection.name,
+      state: mongoose.connection.readyState,
+      server: mongoose.connection.host,
+      port: mongoose.connection.port
+    }
+
+    logger.info("[DB] Conectado ao MongoDB com sucesso! ", info);
   } catch (error) {
     logger.error("[DB] Erro ao conectar ao MongoDB: ", error);
     process.exit(1); // Encerra a aplicação em caso de falha na conexão
