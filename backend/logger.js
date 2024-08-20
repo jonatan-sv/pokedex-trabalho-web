7b66e3093bce768c65f90e6443f9ec43b6487b54
jonatan-sv <jonatan.vidal@alu.ufc.br>
2024-08-20 01:30:31 -0300

    style(backend): estiliza o formato para logger

--- backend/logger.js
+++ backend/logger.js
@@ -1,10 +1,21 @@
 import winston from "winston";
 
+const customFormat = winston.format.printf(
+  ({ level, message, timestamp, ...metadata }) => {
+    let log = `${timestamp} [${level.toUpperCase()}] ${message}`;
+    if (metadata) {
+      log += `${JSON.stringify(metadata)}`;
+    }
+    return log;
+  }
+);
+
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json()
+    winston.format.json(),
+    customFormat
   ),
   transports: [
     new winston.transports.File({ filename: "logs/error.log", level: "error" }),
